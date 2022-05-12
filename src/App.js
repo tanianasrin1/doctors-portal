b09f9bea64b78cf09da3954398bc2ad12ced005c
tanianasrin1 <tasrineva169@gmail.com>
2022-05-12 12:36:45 +0600

    nabver route fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 import Navber from './Pages/Shared/Navber';
 
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/about" element={<About></About>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
         </Routes>  
          
     </div>
---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,26 @@
+import React from "react";
+import chair from '../../assets/images/chair.png'
+
+const Banner = () => {
+  return (
+    <div class="hero min-h-screen">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={chair}
+          class="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div>
+          <h1 class="text-5xl font-bold">Your New Smile Start Here</h1>
+          <p class="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary-500 to-promary-500">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
