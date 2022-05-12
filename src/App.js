74b8fb33a4293ce34725def88bc9c20892833877
tanianasrin1 <tasrineva169@gmail.com>
2022-05-12 11:40:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navber from './Pages/Shared/Navber';
+
 
 function App() {
   return (
     <div>
-         <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
-         <button class="btn btn-primary">Button</button>
+        <Navber></Navber>
 
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+        </Routes>  
          
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#37cdbe",
+          neutral: "#3A4256",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
