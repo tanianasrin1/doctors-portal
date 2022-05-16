43acef9d66d2bdc4fbf67a8763e9d131b29554d6
tanianasrin1 <tasrineva169@gmail.com>
2022-05-16 13:31:45 +0600

    login with google

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -8,7 +8,7 @@ const AvailableAppointment = ({date}) => {
     const [ treatment, setTreatment ] = useState(null)
 
     useEffect( () => {
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
         .then(res => res.json())
         .then(data =>  setServices(data));
     } , [])
@@ -24,7 +24,11 @@ const AvailableAppointment = ({date}) => {
                       ></Service>)
                   }
             </div>
-            {treatment && <BookingModal treatment = {treatment}></BookingModal>}
+            {treatment && <BookingModal 
+            date={date} 
+            treatment = {treatment}
+            setTreatment = {setTreatment}
+            ></BookingModal>}
         </div>
     );
 };
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>this is login</h1>
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user)
+    }
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-center font-bold text-2xl">Login</h2>
+          <div class="divider">OR</div>
+          <button 
+          onClick={() => signInWithGoogle()}
+          class="btn btn-outline"
+          >Continue with Google</button>
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
