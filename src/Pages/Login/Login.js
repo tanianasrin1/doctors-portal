022b30bb89ff389b0377239bb171d510382eaf90
tanianasrin1 <tasrineva169@gmail.com>
2022-05-16 19:40:09 +0600

    login with google

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,23 +1,110 @@
 import React from "react";
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
+import Loading from "../Shared/Loading";
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    if(user){
-        console.log(user)
-    }
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+
+ let signInError;
+
+  if(loading || gLoading){
+      return <Loading></Loading>
+
+  }
+  if(error || gError ){
+      signInError= <p className="text-red-500">{error?.message || gError?.message}</p>
+  }
+  if (gUser) {
+    console.log(gUser);
+  }
+  const onSubmit = (data) => {
+    console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
+  };
   return (
     <div className="flex h-screen justify-center items-center">
       <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="text-center font-bold text-2xl">Login</h2>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+                
+              </label>
+              <input
+                type="email"
+                placeholder="Your email"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                    required: {
+                        value: true,
+                        message: 'Email is Required'
+
+                    },
+                    pattern: {
+                      value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                      message: 'Provide a valid Email' 
+                    }
+                  })}
+              />
+              <label class="label">
+                  {errors.email?.type === 'required' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+                  {errors.email?.type === 'pattern' && <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+              </label>
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Password</span>
+                
+              </label>
+              <input
+                type="password"
+                placeholder="Your password"
+                class="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                    required: {
+                        value: true,
+                        message: 'Password is Required'
+
+                    },
+                    pattern: {
+                      value: 6,
+                      message: 'Must be 6 characters or longer ' 
+                    }
+                  })}
+              />
+              <label class="label">
+                  {errors.password?.type === 'required' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                  {errors.password?.type === 'minLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+              </label>
+            </div>
+            
+            
+            {signInError}
+            <input className="btn w-full max-w-xs" type="submit"  value="Login"/>
+          </form>
+
           <div class="divider">OR</div>
-          <button 
-          onClick={() => signInWithGoogle()}
-          class="btn btn-outline"
-          >Continue with Google</button>
-          
+          <button onClick={() => signInWithGoogle()} class="btn btn-outline">
+            Continue with Google
+          </button>
         </div>
       </div>
     </div>
